d3c07b96275fa7656c6749a0f377631ff2eb0318
ByRojo <67397145+ByRojo@users.noreply.github.com>
2020-08-09 14:09:56 -0500

    Update eliminar_marca.php

--- scripts/eliminar_marca.php
+++ scripts/eliminar_marca.php
@@ -4,14 +4,14 @@ require('db_connection.php');
 
 $idMarca = $_GET['id'];
 
-$cmd = "DELETE FROM MARCA  WHERE marca.id = '$idMarca'";
+$cmd = "UPDATE marca SET estado= 'inactivo'  WHERE marca.id = '$idMarca'";
 
 if ($query = $mysqli->query($cmd)) {
-	header('Location:../admin/marca.php?mensajeEliminarMarca=1');
+	header('Location:../admin/marca.php?mensajeEliminar=1');
 } else {
 	$error = $mysqli->error;
 	$mysqli->close();
-	header('Location:../admin/marca.php?mensajeEliminarMarca=2'.$error);
+	header('Location:../admin/marca.php?mensajeEliminar=2'.$error);
 }
 
 ?>
