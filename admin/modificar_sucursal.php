7bb34e19f87916d4f1d86ff4647fb90a9112617d
ByRojo <67397145+ByRojo@users.noreply.github.com>
2020-08-09 13:52:10 -0500

    Update modificar_sucursal.php

--- admin/modificar_sucursal.php
+++ admin/modificar_sucursal.php
@@ -81,198 +81,227 @@ if (!isset($_GET['id'])) {
 
           <!-- Page Heading -->
           <h1 class="h3 mb-4 text-gray-800 text-center"><i class="fas fa-store"></i> Modificar sucursal</h1>
-          <span class="focus">Llenar los campos correspondientes</span>
+          
+          <?php if(isset( $_GET['mensajeModificar'])) : ?>
+            <?php switch ( $_GET['mensajeModificar'] ) : case 2: ?>
+              <div class="alert alert-danger alert-dismissible fade show mt-4" role="alert">
+                <strong><i class="fas fa-times-circle"></i> Los datosd de la scursal no pudieron ser modificados.</strong> <br/>
+                <span>Verifique que los datos del la sucursal sean correctos.</span> 
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <?php break;?>
+              <?php case 3: ?>
+              <div class="alert alert-danger alert-dismissible fade show mt-4" role="alert">
+                <strong><i class="fas fa-times-circle"></i> El usuario no pudo ser modificado.</strong> <br/>
+                <span>Verifique que los datos de la dirección sean correctos.</span> 
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>     
+              <?php break;?>   
+              <?php case 4: ?>
+              <div class="alert alert-danger alert-dismissible fade show mt-4" role="alert">
+                <strong><i class="fas fa-times-circle"></i> La sucursal no pudo ser modificado.</strong> <br/>
+                <span>Verifique con un administrador el error.</span> 
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>   
+              <?php break; endswitch; ?>
+            <?php endif; ?>
 
 
-          <form action="../scripts/modificar_sucursal.php" method="post" class="pt-4">
-            <div class="form-group">
+            <form action="../scripts/modificar_sucursal.php" method="post" class="pt-4">
+              <div class="form-group">
+                <div class="row">
+                  <div class="col-sm-1"><label for="id">ID. de sucursal</label></div>
+                  <div class="col-sm-2">
+                    <input type="text" name="id" id="id" value="<?=$sucursal_datos['id']?>" class="form-control" readonly>
+                  </div>
+                </div>
+              </div>
+
+              <h4 class="h4 mb-4 text-gray-800">Direccion</h4>
               <div class="row">
-                <div class="col-sm-1"><label for="id">ID. de sucursal</label></div>
-                <div class="col-sm-2">
-                  <input type="text" name="id" id="id" value="<?=$sucursal_datos['id']?>" class="form-control" readonly>
+                <div class="col-sm-6">
+                  <div class="form-group">
+                    <label for="calle">*Calle</label>
+                    <input type="text" name="calle" id="calle" class="form-control" value="<?=$sucursal_datos['calle']?>" maxlength="25" required>
+                  </div>
+                </div>
+                <div class="col-sm-3">
+                  <div class="form-group">
+                    <label for="nE">*No. exterior</label>
+                    <input type="text" name="nE" id="nE" class="form-control" value="<?=$sucursal_datos['numeroExterior']?>" maxlength="10" required>
+                  </div>
+                </div>
+                <div class="col-sm-3">
+                  <div class="form-group">
+                    <label for="nI">*No. interior</label>
+                    <input type="text" name="nI" id="nI" class="form-control" value="<?=$sucursal_datos['numeroInterior']?>" maxlength="10">
+                  </div>
                 </div>
               </div>
-            </div>
+              <div class="row form-group">
+                <div class="col-sm-3">
+                  <div class="form-group">
+                    <label for="col">*Colonia</label>
+                    <input type="text" name="col" id="col" class="form-control" value="<?=$sucursal_datos['colonia']?>" maxlength="40" required>
+                  </div>
+                </div>
+                <div class="col-sm-3">
+                  <div class="form-group">
+                    <label for="cp">*Código postal</label>
+                    <input type="text" name="cp" id="cp" class="form-control" value="<?=$sucursal_datos['codigoPostal']?>" maxlength="10" required>
+                  </div>
+                </div>
+                <div class="col-sm-3">
+                  <div class="form-group">
+                    <label for="edo">*Estado</label>
+                    <select name="edo" id="edo" class="form-control" required>
+                      <option hidden selected value="">Seleccione un estado</option>
+                      <?php 
+                      $cmd = "SELECT * FROM estado";
+                      $query = $mysqli->query($cmd);
+                      
+                      if ($query->num_rows > 0):
+                        while($row = $query->fetch_array(MYSQLI_ASSOC)) :
+                          ?>
+                          <option value="<?=$row['id']?>"><?=$row['nombre']?></option>
+                          <?php 
+                        endwhile;
+                      endif; 
+                      ?>
+                    </select>
+                  </div>
+                </div>  
+                <div class="col-sm-3">
+                  <div class="form-group">
+                    <label for='mun'>*Municipio</label>
+                    <select class="form-control" id="mun" name="mun" required> </select>
+                  </div>
+                </div>
+              </div>
 
-            <h4 class="h4 mb-4 text-gray-800">Direccion</h4>
-            <div class="row">
-              <div class="col-sm-6">
-                <div class="form-group">
-                  <label for="calle">*Calle</label>
-                  <input type="text" name="calle" id="calle" class="form-control" value="<?=$sucursal_datos['calle']?>" required>
+              <h4 class="h4 mb-4 text-gray-800">Datos de Sucursal</h4>
+              <div class="row">
+                <div class="col-sm-3">
+                  <div class="form-group">
+                    <label for="nom1">*Nombre de la sucursal</label>
+                    <input type="text" name="nom1" id="nom1" class="form-control" value="<?=$sucursal_datos['nombre']?>" maxlength="20" required >
+                  </div>
                 </div>
               </div>
-              <div class="col-sm-3">
-                <div class="form-group">
-                  <label for="nE">*No. exterior</label>
-                  <input type="text" name="nE" id="nE" class="form-control" value="<?=$sucursal_datos['numeroExterior']?>" required>
-                </div>
-              </div>
-              <div class="col-sm-3">
-                <div class="form-group">
-                  <label for="nI">*No. interior</label>
-                  <input type="text" name="nI" id="nI" class="form-control" value="<?=$sucursal_datos['numeroInterior']?>">
-                </div>
-              </div>
-            </div>
-            <div class="row form-group">
-              <div class="col-sm-3">
-                <div class="form-group">
-                  <label for="col">*Colonia</label>
-                  <input type="text" name="col" id="col" class="form-control" value="<?=$sucursal_datos['colonia']?>" required>
-                </div>
-              </div>
-              <div class="col-sm-3">
-                <div class="form-group">
-                  <label for="cp">*Código postal</label>
-                  <input type="text" name="cp" id="cp" class="form-control" value="<?=$sucursal_datos['codigoPostal']?>" required>
-                </div>
-              </div>
-              <div class="col-sm-3">
-                <div class="form-group">
-                  <label for="edo">*Estado</label>
-                  <select name="edo" id="edo" class="form-control" required>
-                    <option hidden selected value="">Seleccione un estado</option>
-                    <?php 
-                    $cmd = "SELECT * FROM estado";
-                    $query = $mysqli->query($cmd);
-                    
-                    if ($query->num_rows > 0):
-                      while($row = $query->fetch_array(MYSQLI_ASSOC)) :
-                        ?>
-                        <option value="<?=$row['id']?>"><?=$row['nombre']?></option>
-                        <?php 
-                      endwhile;
-                    endif; 
-                    ?>
-                  </select>
-                </div>
-              </div>  
-              <div class="col-sm-3">
-                <div class="form-group">
-                  <label for='mun'>*Municipio</label>
-                  <select class="form-control" id="mun" name="mun" required> </select>
-                </div>
-              </div>
-            </div>
-
-            <h4 class="h4 mb-4 text-gray-800">Datos de Sucursal</h4>
-            <div class="row">
-              <div class="col-sm-3">
-                <div class="form-group">
-                  <label for="nom1">*Nombre de la sucursal</label>
-                  <input type="text" name="nom1" id="nom1" class="form-control" value="<?=$sucursal_datos['nombre']?>" required >
-                </div>
-              </div>
-            </div>
 
 
-            <div id="suc"></div>
+              <div id="suc"></div>
 
-            <div class="form-group text-center pt-3">
-              <button type="submit" class="btn btn-lg golden-button-primary">
-                <i class="fas fa-check"></i>
-                Guardar cambios
-              </button>
-              <button type="button" class="btn btn-danger btn-lg" onclick="regresar();">
-                <i class="fas fa-times"></i>
-                Cancelar
-              </button>
-            </div>
-          </form>
+              <div class="form-group text-center pt-3">
+                <button type="submit" class="btn btn-lg golden-button-primary">
+                  <i class="fas fa-check"></i>
+                  Guardar cambios
+                </button>
+                <button type="button" class="btn golden-button-danger btn-lg" onclick="regresar();">
+                  <i class="fas fa-times"></i>
+                  Cancelar
+                </button>
+              </div>
+            </form>
+
+          </div>
+          <!-- /.container-fluid -->
 
         </div>
-        <!-- /.container-fluid -->
+        <!-- End of Main Content -->
+
+        <!-- Footer -->
+        <footer class="sticky-footer bg-white">
+          <div class="container my-auto">
+            <div class="copyright text-center my-auto">
+              <span>Copyright &copy; Your Website 2020</span>
+            </div>
+          </div>
+        </footer>
+        <!-- End of Footer -->
 
       </div>
-      <!-- End of Main Content -->
-
-      <!-- Footer -->
-      <footer class="sticky-footer bg-white">
-        <div class="container my-auto">
-          <div class="copyright text-center my-auto">
-            <span>Copyright &copy; Your Website 2020</span>
-          </div>
-        </div>
-      </footer>
-      <!-- End of Footer -->
+      <!-- End of Content Wrapper -->
 
     </div>
-    <!-- End of Content Wrapper -->
+    <!-- End of Page Wrapper -->
 
-  </div>
-  <!-- End of Page Wrapper -->
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top">
+      <i class="fas fa-angle-up"></i>
+    </a>
 
-  <!-- Scroll to Top Button-->
-  <a class="scroll-to-top rounded" href="#page-top">
-    <i class="fas fa-angle-up"></i>
-  </a>
+    <!-- Bootstrap core JavaScript-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-  <!-- Bootstrap core JavaScript-->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- Core plugin JavaScript-->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
 
-  <!-- Core plugin JavaScript-->
-  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-
-  <!-- Custom scripts for all pages-->
-  <script src="js/sb-admin-2.min.js"></script>
-  <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc="
-  crossorigin="anonymous"></script>
-  <script>
-    function regresar() {
-      if (confirm("¿Realmente quiere salir de esta sección?\nNo se guardarán los cambios.")) {
-        window.location.href = 'sucursal.php';
+    <!-- Custom scripts for all pages-->
+    <script src="js/sb-admin-2.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc="
+    crossorigin="anonymous"></script>
+    <script>
+      function regresar() {
+        if (confirm("¿Realmente quiere salir de esta sección?\nNo se guardarán los cambios.")) {
+          window.location.href = 'sucursal.php';
+        }
       }
-    }
+    </script>
+
+  </body>
+
+  </html>
+
+  <script type="text/javascript">
+    $(document).ready(function () {
+      recargarLista();
+
+      $('#edo').change(function () {
+        recargarLista();
+      });
+    });
   </script>
 
-</body>
+  <script type="text/javascript">
+    function recargarLista() {
+      $.ajax({
+        type: "POST",
+        url: "../scripts/municipios.php",
+        data: "estado=" + $('#edo').val(),
+        success: function (response) {
+          $('#mun').html(response);
+        }
+      });
+    } 
+  </script>
 
-</html>
-
-<script type="text/javascript">
-  $(document).ready(function () {
-    recargarLista();
-
-    $('#edo').change(function () {
-      recargarLista();
-    });
-  });
-</script>
-
-<script type="text/javascript">
-  function recargarLista() {
-    $.ajax({
-      type: "POST",
-      url: "../scripts/municipios.php",
-      data: "estado=" + $('#edo').val(),
-      success: function (response) {
-        $('#mun').html(response);
-      }
-    });
-  } 
-</script>
-
-<script>
-  $(document).ready(function () {
-    mostrarSucursales();
-
-    $('#tipo').change(function () {
+  <script>
+    $(document).ready(function () {
       mostrarSucursales();
-    });
-  });
-</script>
 
-<script type="text/javascript">
-  function mostrarSucursales() {
-    $.ajax({
-      type: "POST",
-      url: "../scripts/mostrar_sucursales.php",
-      data: "tipoUsuario=" + $('#tipo').val(),
-      success: function (response) {
-        $('#suc').html(response);
-      }
+      $('#tipo').change(function () {
+        mostrarSucursales();
+      });
     });
-  }
-</script>
+  </script>
+
+  <script type="text/javascript">
+    function mostrarSucursales() {
+      $.ajax({
+        type: "POST",
+        url: "../scripts/mostrar_sucursales.php",
+        data: "tipoUsuario=" + $('#tipo').val(),
+        success: function (response) {
+          $('#suc').html(response);
+        }
+      });
+    }
+  </script>
